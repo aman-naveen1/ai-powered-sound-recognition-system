05d8f3735316f10fe52c10e70e2dcf0aecce4809
Aman Naveen <aman.naveen@ramanujan.du.ac.in>
2025-03-21 19:20:22 +0530

    Create utils.cpp

---
+++ main/src/utils.cpp
@@ -0,0 +1,11 @@
+#include "utils.h"
+#include <iostream>
+
+void Utils::printAsciiBanner() {
+    std::cout << "  _______  _____  _______ \n";
+    std::cout << " |__   __||  __ \\|__   __|\n";
+    std::cout << "    | |   | |__) |  | |   \n";
+    std::cout << "    | |   |  _  /   | |   \n";
+    std::cout << "    | |   | | \\ \\   | |   \n";
+    std::cout << "    |_|   |_|  \\_\\  |_|   \n";
+}
